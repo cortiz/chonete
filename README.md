950cb2e6d0ae26c88f52867d122f03cf00b2f2ab
Carlos Ortiz <chrono.dark@gmail.com>
2018-01-05 10:22:02 -0600

    intial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Simple HTTP
+
+One Paragraph of project description goes here
+
+[![codebeat badge](https://codebeat.co/badges/dc8a4180-51f1-4140-a131-75e85c5d1365)](https://codebeat.co/projects/github-com-cortiz-chonete-develop)
+[![Build Status](https://travis-ci.org/cortiz/chonete.svg?branch=develop)](https://travis-ci.org/cortiz/chonete)
+[![codecov](https://codecov.io/gh/cortiz/chonete/branch/develop/graph/badge.svg)](https://codecov.io/gh/cortiz/chonete)
+
+## Getting Started
+
+
+### Prerequisites
+
+* Java 8
+
+## Authors
+
+* **Carlos Ortiz** - *Initial work* - [PurpleBooth](https://github.com/cortiz)
+
+See also the list of [contributors](https://github.com/cortiz/chonete/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the GPL3 License - see the [LICENSE](LICENSE) file for details
---
+++ src/main/java/org/jmpeax/cli/Chonente.java
@@ -0,0 +1,43 @@
+/**
+ * Chonete
+ * Copyright (C) 2018 Carlos Ortiz.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jmpeax.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class entry point.
+ */
+//CHECKSTYLE:OFF
+public class Chonente {
+//CHECKSTYLE:ON
+    /**
+     * Default hidden Ctr.
+     */
+    public Chonente() {
+        //do not init chonente.
+    }
+
+    /**
+     * Main entry point.
+     * @param args Args to use for startup.
+     */
+    public static void main(final String[] args) {
+
+    }
+}
